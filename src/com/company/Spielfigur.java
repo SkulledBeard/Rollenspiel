f138c36829d4909c52ee1d618a67b32f21967e6a
SkulledBeard <the_rest_in_person@hotmail.com>
2021-09-29 13:30:14 +0200

    Update

--- src/com/company/Spielfigur.java
+++ src/com/company/Spielfigur.java
@@ -1,4 +1,12 @@
 package com.company;
 
 public interface Spielfigur {
+    String name = null;
+    String geschlecht = null;
+    double groesse = 0;
+    int ausdauer = 0;
+    int schnelligkeit = 0;
+    int schlagkraft = 0;
+    int ruestungssaerke = 0;
+
 }
